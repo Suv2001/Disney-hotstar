e35922ad776d3d1b427b22a1a247467d84fd81a6
Suvendu Dash Adhikary <suvendudashadhikary@gmail.com>
2025-03-30 02:01:01 +0530

    refactor(Header): enhance responsiveness and toggle functionality in header component

--- src/Components/HeaderItems.jsx
+++ src/Components/HeaderItems.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 
-function HeaderItems({name, icon}) {
+function HeaderItems({name, icon, children}) {
   return (
-      <div
-          className={'text-uppercase flex items-center gap-3 text-[15px] font-semibold cursor-pointer hover:underline underline-offset-8  hover:underline-width-2'}>
-    <icon>{icon}</icon>
-    <h2>{name.toUpperCase()}</h2>
-</div>
-)
+    <div className={'text-uppercase mb-3 flex items-center gap-3 text-[15px] font-semibold cursor-pointer hover:underline underline-offset-8 hover:underline-width-2'}>
+      {icon}
+      {children || <h2 className={'hidden md:block'}>{name.toUpperCase()}</h2>}
+    </div>
+  )
 }
 
 export default HeaderItems
